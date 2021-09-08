5e75562c77f1c1c9d7b09059468fb64572f64777
Dylan Decker <dylaned@hawaii.edu>
2021-09-07 21:25:02 -1000

    Landing Page WIP
    initial commit for landing page design

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,15 +5,39 @@ import { Grid, Image } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
+      <Grid id='landing-page' verticalAlign='middle' container>
 
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <Image size='small' circular src="https://choosingwiselycanada.org/wp-content/uploads/2020/11/COVID-19_2.png"/>
+          </Grid.Column>
 
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
+          <Grid.Column width={12} textAlign='center'>
+            <h1>The COVID Tracker Application</h1>
+            <p>Track the COVID status of users, with ease</p>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column>
+            <h2>Features</h2>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Column fluid>
+
+          <Grid.Row>
+            <h3>Page 1</h3>
+          </Grid.Row>
+
+          <Grid.Row>
+            <h3>Page 2</h3>
+          </Grid.Row>
+
+          <Grid.Row>
+            <h3>Page 3</h3>
+          </Grid.Row>
+
         </Grid.Column>
 
       </Grid>
